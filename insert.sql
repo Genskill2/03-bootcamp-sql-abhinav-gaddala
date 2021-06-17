e6ae7ef4f983d81e8a4de10244b1cf2f613512f6
abhinav-gaddala <84782654+abhinav-gaddala@users.noreply.github.com>
2021-06-17 19:56:55 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -31,7 +31,7 @@ INSERT into books_subjects(book,subject) values (1,3);
 INSERT into books_subjects(book,subject) values (2,4);
 INSERT into books_subjects(book,subject) values (2,3);
 INSERT into books_subjects(book,subject) values (3,2);
-INSERT into books_subjects(book,subject) values (3,3);
+INSERT into books_subjects(book,subject) values (3,4);
 INSERT into books_subjects(book,subject) values (4,3);
 INSERT into books_subjects(book,subject) values (4,5);
 INSERT into books_subjects(book,subject) values (5,3);
@@ -39,7 +39,7 @@ INSERT into books_subjects(book,subject) values (5,6);
 INSERT into books_subjects(book,subject) values (6,6);
 INSERT into books_subjects(book,subject) values (6,7);
 INSERT into books_subjects(book,subject) values (7,5);
-INSERT into books_subjects(book,subject) values (7,9);
+INSERT into books_subjects(book,subject) values (7,8);
 INSERT into books_subjects(book,subject) values (8,8);
 INSERT into books_subjects(book,subject) values (8,9);
 
