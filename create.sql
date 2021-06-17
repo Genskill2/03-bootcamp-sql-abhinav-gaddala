2ee77b35fa7271a3b7fc31587a04de6af4fdf0b8
abhinav-gaddala <84782654+abhinav-gaddala@users.noreply.github.com>
2021-06-17 19:25:44 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,24 +1,24 @@
-create table publisher (
-	id INTEGER primary key autoincrement,
-	name text,
-	country text
-	);
-	
-create table books (
-	id INTEGER primary key autoincrement,
-	title text,
-	publisher INTEGER,
-	foriegn key (publisher) references publisher(id)
-	);
-	
-create table subjects (
-	id INTEGER primary key autoincrement,
-	name text
-	);
+CREATE TABLE publisher (
+    id INTEGER PRIMARY KEY AUTOINCREMENT ,
+    name TEXT,
+    country TEXT
+);
 
-create table books_subjects (
-	book INTEGER,
-	subject INTEGER,
-	foriegn key (book) references books(id),
-	foriegn key (subject) references subjects(id),
-	);
+CREATE TABLE books(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    publisher INTEGER  ,
+    FOREIGN KEY (publisher) REFERENCES publisher(id)
+);
+
+CREATE TABLE subjects(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE books_subjects(
+    book INTEGER ,
+    subject INTEGER,
+    FOREIGN KEY (book) REFERENCES books(id),
+    FOREIGN KEY (subject) REFERENCES subjects(id)
+);
