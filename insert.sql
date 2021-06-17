2d261f5e88d1e6aee2762a8867b2290b5e3ab06b
abhinav-gaddala <84782654+abhinav-gaddala@users.noreply.github.com>
2021-06-17 19:21:17 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -16,32 +16,32 @@ INSERT into subjects(name) values ("Politics");
 INSERT into subjects(name) values ("History");
 
 
-INSERT into books(title,publisher) values ("The C Programming Language","1");
-INSERT into books(title,publisher) values ("The Go Programming Language","1");
-INSERT into books(title,publisher) values ("The UNIX Programming Environment","1");
-INSERT into books(title,publisher) values ("Cryptonomicon","2");
-INSERT into books(title,publisher) values ("Deep Work","3");
-INSERT into books(title,publisher) values ("Atomic Habits","4");
-INSERT into books(title,publisher) values ("The City and The City","5");
-INSERT into books(title,publisher) values ("The Great War for Civilisation","6");
+INSERT into books(title,publisher) values ("The C Programming Language",1);
+INSERT into books(title,publisher) values ("The Go Programming Language",1);
+INSERT into books(title,publisher) values ("The UNIX Programming Environment",1);
+INSERT into books(title,publisher) values ("Cryptonomicon",2);
+INSERT into books(title,publisher) values ("Deep Work",3);
+INSERT into books(title,publisher) values ("Atomic Habits",4);
+INSERT into books(title,publisher) values ("The City and The City",5);
+INSERT into books(title,publisher) values ("The Great War for Civilisation",6);
 
-INSERT into books_subjects(book,subject) values ("1","1");
-INSERT into books_subjects(book,subject) values ("1","2");
-INSERT into books_subjects(book,subject) values ("1","3");
-INSERT into books_subjects(book,subject) values ("2","4");
-INSERT into books_subjects(book,subject) values ("2","3");
-INSERT into books_subjects(book,subject) values ("3","2");
-INSERT into books_subjects(book,subject) values ("3","3");
-INSERT into books_subjects(book,subject) values ("4","3");
-INSERT into books_subjects(book,subject) values ("4","5");
-INSERT into books_subjects(book,subject) values ("5","3");
-INSERT into books_subjects(book,subject) values ("5","6");
-INSERT into books_subjects(book,subject) values ("6","6");
-INSERT into books_subjects(book,subject) values ("6","7");
-INSERT into books_subjects(book,subject) values ("7","8");
-INSERT into books_subjects(book,subject) values ("7","9");
-INSERT into books_subjects(book,subject) values ("8","9");
-INSERT into books_subjects(book,subject) values ("8","10");
+INSERT into books_subjects(book,subject) values (1,1);
+INSERT into books_subjects(book,subject) values (1,2);
+INSERT into books_subjects(book,subject) values (1,3);
+INSERT into books_subjects(book,subject) values (2,4);
+INSERT into books_subjects(book,subject) values (2,3);
+INSERT into books_subjects(book,subject) values (3,2);
+INSERT into books_subjects(book,subject) values (3,3);
+INSERT into books_subjects(book,subject) values (4,3);
+INSERT into books_subjects(book,subject) values (4,5);
+INSERT into books_subjects(book,subject) values (5,3);
+INSERT into books_subjects(book,subject) values (5,6);
+INSERT into books_subjects(book,subject) values (6,6);
+INSERT into books_subjects(book,subject) values (6,7);
+INSERT into books_subjects(book,subject) values (7,8);
+INSERT into books_subjects(book,subject) values (7,9);
+INSERT into books_subjects(book,subject) values (8,9);
+INSERT into books_subjects(book,subject) values (8,10);
 
 
 
